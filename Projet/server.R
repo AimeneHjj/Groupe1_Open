6afd976eeb3eb82a80a8afa7980f2f7a51f81dc8
ami-69340 <gokusa2100@gmail.com>
2026-02-10 14:23:29 +0100

    Ajout sur interface apprentissage : "mot à traduire", "La réponse est :"

--- Projet/server.R
+++ Projet/server.R
@@ -2,7 +2,7 @@ library(shiny)
 
 # Define server logic
 function(input, output, session) {
+  output$Traduction <- renderText(print("Mot à traduire : "))
+  output$Réponse <- renderText(print("La réponse est : "))
+  }
   
-  # Ton code serveur ici
-  
-}
