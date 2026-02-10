be9f2c63ac4cb71582ab209dd64167f9515d2a0c
louis-rmr <lromero@etu.isara.fr>
2026-02-10 10:27:03 +0100

    ajout contenu de l'onglet parmétrage

--- ApplicationAïmène/ui.R
+++ ApplicationAïmène/ui.R
@@ -14,7 +14,18 @@ fluidPage(
       tabPanel("Apprentissage",
                "Contenu de l'onglet Apprentissage")
       tabPanel("Paramétrage",
-               "Contenu de l'onglet Paramétrage")
+                fluidPage(
+                h3("Ajouter un mot"), #affiche le titre
+                textInput("mot_fr", "Mot"), #zone d'écriture du mot en français
+                textInput("mot_et", "Mot a traduire"),# zone d'écriture du mot traduit
+                textInput("nouvelle_categorie", "Nouvelle catégorie"), # zone d'écriture de la catégorie
+                selectInput("categorie_existante", "Catégorie",
+                              choices = c("Animaux", "Verbes", "Objets")),# liste déroulante catégorie
+                textInput("nouvelle_langue", "Nouvelle langue"),#zone d'écriture de la langue 
+                selectInput("langue", "Langue",
+                              choices = c("Anglais")),# liste déroulante de la langue 
+                actionButton("Valider_mot", "Valider") # bonton pour valider
+                        ))
       tabPanel("Crédits","Contenu de l'onglet Crédits")
     )
   )
